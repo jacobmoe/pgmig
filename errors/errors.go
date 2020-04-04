f749619bd9280ada029e87e946bf80e72aaab152
Jacob Moeller <jacobmoe@gmail.com>
2020-04-04 13:51:27 -0400

    remove unused error

--- errors/errors.go
+++ errors/errors.go
@@ -1,18 +1,5 @@
 package errors
 
-// ErrUninitialized is an initialization error
-type ErrUninitialized struct{}
-
-// NewErrUninitialized returns a new ErrUninitialized
-func NewErrUninitialized() *ErrUninitialized {
-	return &ErrUninitialized{}
-}
-
-// Error returns the error message
-func (e *ErrUninitialized) Error() string {
-	return "must call Init use"
-}
-
 // ErrSQLLoad is an error loading a SQL file
 type ErrSQLLoad struct {
 	message string
